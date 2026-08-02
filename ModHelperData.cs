eaa27e0530b88e5f022365239853d24359b12124
hubor <minecraftcostam2009@gmail.com>
2026-08-02 18:28:56 +0200

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,13 +2,13 @@ namespace armybase;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "43.0";
-    public const string Version = "2.0.0";
+    public const string WorksOnVersion = "55.0";
+    public const string Version = "2.0.1";
     public const string Name = "Army Base";
 
-    public const string Description = "Adds a new custom Army Base tower. Complete with all 15 tiers and a Paragon coming soon!";
+    public const string Description = "Adds a new custom Army Base tower. Complete with all 15 tiers, and a Paragon and 555 version available now on Tewtiy's membership server!";
 
-    public const string RepoOwner = "hubor0"; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = "armybase"; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "hubor0";
+    public const string RepoName = "armybase";
     public const string Author = "hubor";
 }
